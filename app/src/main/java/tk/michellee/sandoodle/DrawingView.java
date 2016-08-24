538459492374a3fb4aa8c2fe472e22d7ee1af568
Michelle Lee <tl428@cornell.edu>
2016-08-24 15:58:16 -0400

    First project to get familiar with Android Studio and Git

---
+++ app/src/main/java/tk/michellee/sandoodle/DrawingView.java
@@ -0,0 +1,190 @@
+package tk.michellee.sandoodle;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Created by michelle on 8/14/16.
+ * A test project to learn how to make a drawing app on Android Studio
+ * More features will be added in the future, including a menu screen and settings
+ */
+public class DrawingView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xFF660000;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap, sandColor;
+    private boolean erase=false;
+    private float brushSize,lastBrushSize;
+    private Shader shader;
+
+    public DrawingView(Context context, AttributeSet attributeset){
+        super(context, attributeset);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //set up drawing area
+        //set up Path to trace users' drawing action
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(paintColor);
+        drawPaint.setStrokeWidth(brushSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.BUTT);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+
+        brushSize = getResources().getInteger(R.integer.size2);
+        lastBrushSize = brushSize;
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    public void setColor(String newColor) {
+        //set color
+        invalidate();
+
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+
+        if (paintColor == 0xffff0000) {
+            sandColor = BitmapFactory.decodeResource(
+                    getResources(), R.drawable.pink);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff0000ff) {
+            sandColor = BitmapFactory.decodeResource(
+                    getResources(), R.drawable.blue);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff660000) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.brown);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xffff6600) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.orange);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xffffcc00) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.sand);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff009900) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.mint);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff009999) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.teal);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff990099) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.purple);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xffff6666) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.green);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xffffffff) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.white);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff787878) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.grey);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        } else if (paintColor == 0xff000000) {
+            sandColor = BitmapFactory.decodeResource(getResources(), R.drawable.black);
+            shader = new BitmapShader(sandColor,
+                    Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        }
+
+        drawPaint.setShader(shader);
+    }
+
+
+    public void setErase(boolean isErase){
+        //set erase true or false;
+        erase = isErase;
+        if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        else drawPaint.setXfermode(null);
+    }
+
+    public void setBrushSize(float newSize){
+        //update size
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize, getResources().getDisplayMetrics());
+        brushSize = pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
+    }
+
+    public void setLastBrushSize(float lastSize){
+        lastBrushSize=lastSize;
+    }
+
+    public float getLastBrushSize(){
+        return lastBrushSize;
+    }
+
+    public void startNew(){
+        drawCanvas.drawColor(0,PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+}
---
+++ app/src/main/java/tk/michellee/sandoodle/MainActivity.java
@@ -0,0 +1,216 @@
+package tk.michellee.sandoodle;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXTRA_MESSAGE = "tk.michelle.sandoodle.MESSAGE";
+    private DrawingView drawView;
+    private ImageButton currentPaint, drawBtn, eraseBtn, newBtn,saveBtn;
+    private float smallBrush, mediumBrush, largeBrush;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currentPaint = (ImageButton)paintLayout.getChildAt(0);
+        currentPaint.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.paint_pressed));
+        eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
+        eraseBtn.setOnClickListener(this);
+
+        smallBrush = getResources().getInteger(R.integer.size1);
+        mediumBrush = getResources().getInteger(R.integer.size2);
+        largeBrush = getResources().getInteger(R.integer.size3);
+
+        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(this);
+
+        newBtn = (ImageButton)findViewById(R.id.new_btn);
+        newBtn.setOnClickListener(this);
+
+        saveBtn = (ImageButton)findViewById(R.id.save_btn);
+        saveBtn.setOnClickListener(this);
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currentPaint){
+            //update color
+            ImageButton imgView=(ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            imgView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.paint_pressed));
+            currentPaint.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.paint_pressed));
+            currentPaint=(ImageButton)view;
+            drawView.setBrushSize(drawView.getLastBrushSize());
+        }
+    }
+
+    public void onClick(View view){
+        //responds to clicks
+        if(view.getId() == R.id.draw_btn){
+            //draw button clicked
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
+
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(smallBrush);
+                    drawView.setLastBrushSize(smallBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(mediumBrush);
+                    drawView.setLastBrushSize(mediumBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(largeBrush);
+                    drawView.setLastBrushSize(largeBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+
+                }
+            });
+
+            brushDialog.show();
+        }
+            else if(view.getId()==R.id.erase_btn){
+            //switch to erase - choose size
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Eraser size:");
+
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(smallBrush);
+
+                    brushDialog.dismiss();
+
+                }
+            });
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(mediumBrush);
+
+                    brushDialog.dismiss();
+
+                }
+            });
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    drawView.setErase(true);
+                    drawView.setBrushSize(largeBrush);
+
+                    brushDialog.dismiss();
+
+                }
+            });
+
+
+            brushDialog.show();
+        }
+        else if(view.getId()==R.id.new_btn){
+            //new button
+            AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+            newDialog.setTitle("New drawing");
+            newDialog.setMessage("Start new drawing? Current drawing will not be saved.");
+            newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    drawView.startNew();
+                    dialog.dismiss();
+                }
+            });
+            newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            newDialog.show();
+        } else if(view.getId()==R.id.save_btn){
+            //save drawing
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to your device");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    //save drawing
+                    drawView.setDrawingCacheEnabled(true);
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+
+                    if(imgSaved!=null){
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else{
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Error: Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+
+                    drawView.destroyDrawingCache();
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+
+
+            });
+            saveDialog.show();
+        }
+
+    }
+
+}
